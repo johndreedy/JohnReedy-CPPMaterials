ed9cf1360445db0b8e1e4f4ddeb834844e34f5ed
John Reedy <johnreedy01@gmail.com>
2022-07-15 22:12:02 +0100

    Commit v2

--- Lesson 3 - Using Variables/Lesson 3 - Using Variables.cpp
+++ Lesson 3 - Using Variables/Lesson 3 - Using Variables.cpp
@@ -6,16 +6,17 @@ int main()
 {
 
     string CharacterName = "John";
+    string CharacterGender = "man";
     int CharacterAge = 25;
 
-    cout << "There is a man named " << CharacterName << endl;
-    cout << "He is "<< CharacterAge << " years old" << endl;
+    cout << "There is a " << CharacterGender << " named " << CharacterName << endl;
+    cout << "He is " << CharacterAge << " years old" << endl;
+    cout << "He likes the name " << CharacterName << endl;
+    cout << "But he does not like being " << CharacterAge << endl;
 
     CharacterName = "Joe";
     CharacterAge = 30;
 
-    cout << "He likes the name " << CharacterName << endl;
-    cout << "But he does not like being " << CharacterAge << endl;
 
     return 0;
 }
