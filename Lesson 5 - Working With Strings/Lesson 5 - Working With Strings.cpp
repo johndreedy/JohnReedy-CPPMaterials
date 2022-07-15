c72a5d35b61424e4313d096da2b32b6715a880dc
John Reedy <johnreedy01@gmail.com>
2022-07-15 23:12:57 +0100

    Commit v4 (Final)

--- Lesson 5 - Working With Strings/Lesson 5 - Working With Strings.cpp
+++ Lesson 5 - Working With Strings/Lesson 5 - Working With Strings.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "First line\n";
+    cout << "Second line\n";
+
+    string phrase = "Third line";
+    string phrasesubstr;
+
+    cout << phrase.length() << endl;
+    phrase[0] = 'P';
+    cout << phrase << endl;
+
+    cout << phrase.find("line", 0);
+
+    phrasesubstr = phrase.substr(4, 5);
+    cout << phrasesubstr << endl;
+
     return 0;
 }
