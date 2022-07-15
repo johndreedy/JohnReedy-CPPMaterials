a36c025fc982a63afad12c1c02b697b3b5b26402
John Reedy <johnreedy01@gmail.com>
2022-07-15 22:31:46 +0100

    Commit v3

---
+++ Lesson 4 - Different Data Types/Lesson 4 - Different Data Types.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    char letter = 'A';
+    string phrase = "Hello friends!";
+    int number_01 = 9001;
+    float number_02 = 666;
+    double number_03 = 4.5;
+    bool IsMale = true;
+    bool IsFemale = false;
+
+    cout << letter << phrase << number_01 << number_02 << number_03 << IsMale << IsFemale << endl;
+
+    return 0;
+
+}
