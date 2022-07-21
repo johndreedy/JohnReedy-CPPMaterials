b8f24aa096b862a157c882b6cd1fa932d28ce7b3
John Reedy <johnreedy01@gmail.com>
2022-07-21 18:41:43 +0100

    Latest Commit

--- 01 LearnCPP.com/1.3 - Objects and Variables/1.3 - Objects and Variables.cpp
+++ 01 LearnCPP.com/1.3 - Objects and Variables/1.3 - Objects and Variables.cpp
@@ -2,11 +2,24 @@
 
 int main()
 {
-	int x = 20;
-	double y = 6.66;
+	int x; // ints are whole numbers
+	double y; // doubles (and floats) are fractional numbers
 
-	std::cout << x;
-	std::cout << " \n";
-	std::cout << y;
-	std::cout << " \n";
+	int a, b; 
+	// it's possible to initialise 2 or more variables in the same statement
+	// however, apparently this is not recommended for readability purposes
+
+	x = 20;
+	y = 6.66;
+
+	a = 10;
+	b = 30;
+
+	std::cout << x << std::endl;
+	std::cout << y << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+
+	// I have included print strings and defined the variables for testing purposes
+	// but I will explore this more in lesson 1.4
 }
